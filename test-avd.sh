9136a36b50383de6881cce6c1fe6238be50176ee
Gustavo Gard <gusgard@gmail.com>
2020-12-08 17:00:57 -0300

    v2

--- test-avd.sh
+++ test-avd.sh
@@ -4,3 +4,4 @@ echo no | avdmanager create avd -n testEmulator -k "system-images;android-21;goo
 # curl -sS https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator -o android-wait-for-boot.sh
 # chmod +x ./android-wait-for-boot.sh && ./android-wait-for-boot.sh
 # adb shell input keyevent 82
+emulator -list-avds
